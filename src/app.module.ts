b196e1206d422adeb6922cd311f68b62ba4ca2b8
juanframart2011 <juanframart2011@gmail.com>
2024-01-07 21:51:07 -0600

    entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MensajesController } from './mensajes/mensajes.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'user_course',
+      password: '123456',
+      database: 'nestjs-node-typescript-al-estilo-angular-para-crear-apis',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController, MensajesController],
   providers: [AppService],
 })
